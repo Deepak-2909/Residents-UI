6ba092721ca4bf99ad742b37d5fb14f357df170a
Deepak2909 <sdyadav200@gmail.com>
2021-07-13 16:11:27 +0530

    Initial Commit

---
+++ ResidentsUI/ResidentsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResidentsUIApp.swift
+//  ResidentsUI
+//
+//  Created by Deepak on 13/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ResidentsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
